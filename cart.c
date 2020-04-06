f6b598e71b9b405055aeb89a85fcbfddde435297
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-06 22:59:50 +0200

    Create cart.c

---
+++ cart.c
@@ -0,0 +1,27 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+/*Run with 8 processes*/
+void main (int argc, char *argv[])
+{
+  int rank;
+  MPI_Comm vu;
+  int dim[2], period[2], reorder;
+  int coord[2], id, coordn[2];
+  MPI_Init (&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  dim[0]=4;
+  dim[1]=2;
+  period[0]=1;
+  period[1]=1;
+  reorder=1;
+  MPI_Cart_create(MPI_COMM_WORLD,2,dim,period,reorder,&vu);
+  
+  MPI_Cart_coords(vu,rank,2,coord);
+  coordn[0]=coord[0]+1;
+  coordn[1]=coord[1];
+  MPI_Cart_rank(vu, coordn, &id)	
+  printf("P:%d My neighbour to the right is %d\n", rank, id);
+   
+  MPI_Finalize();
+}
