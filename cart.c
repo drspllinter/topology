cf61b9ee3e008373e31109b94e6a27df7c9ddf5f
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-06 23:10:56 +0200

    Update cart.c

--- cart.c
+++ cart.c
@@ -7,7 +7,7 @@ void main (int argc, char *argv[])
   int rank;
   MPI_Comm vu;
   int dim[2], period[2], reorder;
-  int coord[2], id, coordn[2];
+  int coord[2], idr, idl, idu, idd, coordn[2];
   MPI_Init (&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   dim[0]=4;
@@ -16,12 +16,15 @@ void main (int argc, char *argv[])
   period[1]=1;
   reorder=1;
   MPI_Cart_create(MPI_COMM_WORLD,2,dim,period,reorder,&vu);
-  
   MPI_Cart_coords(vu,rank,2,coord);
-  coordn[0]=coord[0]+1;
+  /*Neighbour to the left*/
+  coordn[0]=coord[0]-1;
   coordn[1]=coord[1];
-  MPI_Cart_rank(vu, coordn, &id)	
-  printf("P:%d My neighbour to the right is %d\n", rank, id);
+  MPI_Cart_rank(vu, coordn, &idl);
+  /*Neighbour to the right*/
+  coordn[0]=coord[0]+1;
+  MPI_Cart_rank(vu, coordn, &idr);	
+  printf("P:%d My neighbour to the left is %d and to the right %d\n", rank, idl, idr);
    
   MPI_Finalize();
 }
